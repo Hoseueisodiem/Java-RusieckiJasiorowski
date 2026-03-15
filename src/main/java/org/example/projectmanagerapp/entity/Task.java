0f30c4432294d049862896c3fe7514218db5ad0a
Patryk <pjasiorowski@gmail.com>
2026-03-15 09:06:44 +0100

    Zadanie 2: encja task i ennum Student_b_Jasiorowski

---
+++ src/main/java/org/example/projectmanagerapp/entity/Task.java
@@ -0,0 +1,31 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity 
+@Table(name = "tasks")
+public class Task {
+
+    @Id // Klucz główny [cite: 14]
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @Column(nullable = false) // Pole nie może być puste [cite: 16]
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING) 
+    private TaskType taskType;
+
+    @ManyToOne // Relacja do projektu (Zadanie 2) [cite: 66, 90]
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne // Relacja do użytkownika (Zadanie 2) [cite: 66]
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    
+}
