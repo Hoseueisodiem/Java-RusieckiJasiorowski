dddfba9e078c73b8fd843a86d08b3fc0fe990429
Hoseueisodiem <filip.rusieckiiii@gmail.com>
2026-03-15 08:37:24 +0100

    Zad 1 Lab 2: Project and Users entities ManyToMany

---
+++ src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -0,0 +1,30 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Set;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_users",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<Users> users;
+}
