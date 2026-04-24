5c107f850d91394b695d3ab11b509ca302d48b43
Patryk <pjasiorowski@gmail.com>
2026-04-24 20:47:25 +0200

    Zadanie 2: Dodanie encji Task z relacjami i getterami

--- src/main/java/org/example/projectmanagerapp/entity/Task.java
+++ src/main/java/org/example/projectmanagerapp/entity/Task.java
@@ -38,51 +38,16 @@ public class Task {
         this.user = user;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public TaskType getTaskType() {
-        return taskType;
-    }
-
-    public void setTaskType(TaskType taskType) {
-        this.taskType = taskType;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
-
-    public Users getUser() {
-        return user;
-    }
-
-    public void setUser(Users user) {
-        this.user = user;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public TaskType getTaskType() { return taskType; }
+    public void setTaskType(TaskType taskType) { this.taskType = taskType; }
+    public Project getProject() { return project; }
+    public void setProject(Project project) { this.project = project; }
+    public Users getUser() { return user; }
+    public void setUser(Users user) { this.user = user; }
 }
